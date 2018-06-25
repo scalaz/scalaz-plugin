f64dee6183dd8d76b841ce55265fa63abcd75613
Alexander Konovalov <alex.knvl@gmail.com>
2018-06-24 20:57:54 -0400

    More tests.

---
+++ plugin/src/test/files/neg/orphan_hk.scala
@@ -0,0 +1,31 @@
+import scalaz.meta.Typeclass
+import scalaz.meta.orphan
+
+import scala._
+
+object orphan_hk {
+  trait Foo[A[_]] extends Typeclass
+  object Foo {
+    // This is okay, typeclass companion.
+    implicit val foo: Foo[List] = new Foo[List] { }
+  }
+
+  class Bar[A]
+  object Bar {
+    // This is okay, type companion.
+    implicit val foo: Foo[Bar] = new Foo[Bar] { }
+  }
+
+  // Not okay, orphan instance.
+  implicit val foo1: Foo[Bar] = new Foo[Bar] { }
+
+  object Other {
+    // Not okay, orphan instance.
+    implicit val foo2: Foo[Bar] = new Foo[Bar] { }
+  }
+
+  object Something {
+    // This is okay, an explicit orphan.
+    @orphan implicit val foo3: Foo[Bar] = new Foo[Bar] { }
+  }
+}
