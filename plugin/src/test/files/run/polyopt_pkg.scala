b83220662ef50fedcb3a4e6d1b058eb5fd5b5272
Alexander Konovalov <alex.knvl@gmail.com>
2018-06-26 12:14:02 -0400

    Package object test.

---
+++ plugin/src/test/files/run/polyopt_pkg.scala
@@ -0,0 +1,25 @@
+class Foo[A]()
+
+package test {
+  object `package` {
+    val boo: Int = 1
+
+    def get[A]: Foo[A] = new Foo()
+
+    def get1[A](a: A): List[A] = List(a)
+  }
+}
+
+
+import java.io.{BufferedReader, InputStreamReader, File}
+
+object Test {
+  def main(args: Array[String]): Unit = {
+    val path = new File("src/test/files/run/polyopt_pkg-run.obj/test/package$.class").getAbsolutePath
+
+    val p = new ProcessBuilder().command("javap", "-c", path)
+      .redirectOutput(ProcessBuilder.Redirect.INHERIT)
+      .redirectError(ProcessBuilder.Redirect.INHERIT)
+      .start().waitFor()
+  }
+}
